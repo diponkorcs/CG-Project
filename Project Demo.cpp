e3e251afb855049a85bef3821c2bf83787d7a035
diponkorcs <diponkorchandro678@gmail.com>
2021-04-23 22:50:16 +0600

    Header files Added

---
+++ Project Demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<GL/gl.h>
+#include <GL/glut.h>
+#include <windows.h>
+#include <math.h>
+using namespace std;
+
+float _moveA = 0.0f;
+float _moveB = 0.0f;
+float _moveC = 0.0f;
+float _angle1 = 0.0f;
+float speed = 0.02f;
